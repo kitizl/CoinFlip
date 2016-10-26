19f5866fe962c889be50d816a565d72611aa951a
kitizl <nithesh.dragoon98@gmail.com>
2016-10-26 01:08:36 -0700

    this version should work

--- js/logic.js
+++ js/logic.js
@@ -1,11 +1,9 @@
-var coin = document.getElementById("coin");
-
 function toss(){
 	var choice = Math.floor(Math.random()*2);
 	switch(choice){
-		case 0: coin.src = "img/heads.jpg";
+		case 0: document.getElementById('coin').src = "img/heads.jpg";
 				break;
-		case 1: coin.src = "img/tails.jpg";
+		case 1: document.getElementById('coin').src = "img/tails.jpg";
 				break;
 		default: console.log('error');
 				break;
